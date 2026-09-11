157a805d8218b10d80e7eb9049711ffdd8dc40a2
lessandrobull <lessandro.bull@gmail.com>
2026-09-11 02:51:36 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Destrava',
+  title: 'Leveld',
   description: 'Plataforma de leitura e escuta guiada de inglês.',
 };
 
